b7fda3ac4f3d59cec526563e8989ae8fe8a5a8bf
yiding he <yidinghe2012@gmail.com>
2016-10-31 13:59:37 -0400

    Chapter 3. Creating a project

--- Kotlin-Android/app/src/main/kotlin/yidinghe/com/android/kotlin/MainActivity.kt
+++ Kotlin-Android/app/src/main/kotlin/yidinghe/com/android/kotlin/MainActivity.kt
@@ -3,15 +3,17 @@ package yidinghe.com.android.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.message_text
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         message_text.text = "Hello Kotlin!"
+        Log.d("KOTLIN", "MainActivity, onCreate")
         toast("hahahha")
     }
 
