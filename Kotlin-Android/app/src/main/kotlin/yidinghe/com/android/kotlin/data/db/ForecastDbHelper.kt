7a866501e9771809ee444a46f23415ae741db27b
yidinghe <yidinghe2012@gmail.com>
2016-11-04 10:57:21 -0400

    Chapter 17. Creating a SQLiteHelper.

---
+++ Kotlin-Android/app/src/main/kotlin/yidinghe/com/android/kotlin/data/db/ForecastDbHelper.kt
@@ -0,0 +1,31 @@
+package yidinghe.com.android.kotlin.data.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+import yidinghe.com.android.kotlin.ui.App
+
+/**
+ * Created by yidinghe on 11/4/16.
+ */
+
+class ForecastDbHelper(ctx: Context = App.instance) : ManagedSQLiteOpenHelper(ctx, ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance by lazy { ForecastDbHelper() }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.createTable(CityForecastTable.NAME, true, CityForecastTable.ID to INTEGER + PRIMARY_KEY, CityForecastTable.CITY to TEXT, CityForecastTable.COUNTRY to TEXT)
+        db?.createTable(DayForecastTable.NAME, true, DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT, DayForecastTable.DATE to INTEGER, DayForecastTable.DESCRIPTION to TEXT, DayForecastTable.HIGH to INTEGER, DayForecastTable.LOW to INTEGER, DayForecastTable.ICON_URL to TEXT, DayForecastTable.CITY_ID to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.dropTable(CityForecastTable.NAME, true)
+        db?.dropTable(DayForecastTable.NAME, true)
+        onCreate(db)
+    }
+
+}
---
+++ Kotlin-Android/app/src/main/kotlin/yidinghe/com/android/kotlin/data/db/Tables.kt
@@ -0,0 +1,23 @@
+package yidinghe.com.android.kotlin.data.db
+
+/**
+ * Created by yidinghe on 11/4/16.
+ */
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
